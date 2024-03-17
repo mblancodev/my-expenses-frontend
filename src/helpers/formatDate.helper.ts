5c217bf976dbcc6c77e196dcf5a685ff0fc98063
Manuel Blanco <manuel.blanco@edgeuno.com>
2024-03-16 22:19:44 -0500

    we should be able to pass a different format to that helper

--- src/helpers/formatDate.helper.ts
+++ src/helpers/formatDate.helper.ts
@@ -1,5 +1,7 @@
 import { DateTime } from "luxon";
 
-export const formatDate = (date: string) => {
-  return `${DateTime.fromJSDate(new Date(date)).toFormat("yyyy LLL dd")}`;
+export const formatDate = (date: string, format?: string) => {
+  return `${DateTime.fromJSDate(new Date(date)).toFormat(
+    format ? format : "yyyy LLL dd"
+  )}`;
 };
