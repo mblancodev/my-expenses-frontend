17e8f5eb5e2268dd44893c47600275f0b47ed136
Manuel Blanco <manuel.blanco@edgeuno.com>
2024-03-16 22:52:33 -0500

    keeping style on every helper

--- src/helpers/checkIsValidDate.helper.ts
+++ src/helpers/checkIsValidDate.helper.ts
@@ -1,4 +1,4 @@
-export function isValidDate(dateString: string) {
+export const isValidDate = (dateString: string) => {
   const date = new Date(dateString);
   // Check if date is invalid
   // @ts-expect-error
@@ -9,4 +9,4 @@ export function isValidDate(dateString: string) {
   // Additional check for formats that are parsed as valid by the Date constructor,
   // but do not correspond to real dates (e.g., February 30)
   return date.toISOString().slice(0, 10) === dateString;
-}
+};
